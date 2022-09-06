0ad143716fd2694431ecc369161c4ffa75e16f08
anatoly <tolyan.yalta@yandex.ru>
2022-09-06 15:13:48 +0300

    Add Task_1. Calculation of numbers entered by the user

---
+++ Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Программа запрашивает натуральное число M, пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//Console.WriteLine("Введите натуральное число: ");     // Не понятно что подразумевается под
+//int value = Convert.ToInt32(Console.ReadLine());      // "Программа запрашивает"?
+                                                        // Закоментарен вариант ввода от пользоватля
+
+int value = new Random().Next(1, 10);
+
+Console.WriteLine("Вводите числа " + value + " раз: ");
+
+int[] array = new int[value];
+
+for(int i = 0; i < value; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int count = 0;
+
+for(int i = 0; i < value; i++)
+{
+    if(array[i] > 0)
+    {
+        count += 1;
+    }
+}
+
+Console.WriteLine("Пользователь ввел " + count + " чисел больше 0");
