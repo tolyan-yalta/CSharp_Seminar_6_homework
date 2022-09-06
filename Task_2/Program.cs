9b746fab1fe80f37c87a5a08114143db5d264655
anatoly <tolyan.yalta@yandex.ru>
2022-09-07 02:01:10 +0300

    Changed Task_2 v2.0

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -36,20 +36,27 @@ double b2 = entering_value_b2();
 double k1 = entering_value_k1();
 double k2 = entering_value_k2();
 
-if(k1 == k2 && b1 == b2)
+bool check1 = false;
+bool check2 = false;
+while(check1 == false || check2 == false)
 {
-    Console.WriteLine("При k1 = k2  и b1 = b2 прямые совпадают, задайте значения заново");
-    b1 = entering_value_b1();
-    b2 = entering_value_b2();
-    k1 = entering_value_k1();
-    k2 = entering_value_k2();
-}
+    if(k1 == k2 && b1 == b2)
+    {
+        Console.WriteLine("При k1 = k2  и b1 = b2 прямые совпадают, задайте значения заново");
+        b1 = entering_value_b1();
+        b2 = entering_value_b2();
+        k1 = entering_value_k1();
+        k2 = entering_value_k2();
+    }
+    else check1 =true;
 
-if(k1 == k2)
-{
-    Console.WriteLine("При k1 = k2 прямые параллельны, задайте значения заново");
-    k1 = entering_value_k1();
-    k2 = entering_value_k2();
+    if(k1 == k2)
+    {
+        Console.WriteLine("При k1 = k2 прямые параллельны, задайте значения заново");
+        k1 = entering_value_k1();
+        k2 = entering_value_k2();   
+    }
+    else check2 = true;
 }
 
 double x = (b2 - b1) / (k1 - k2);
